c92dd58451769c8b1c17ba9a7c35278ee840a914
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2022-09-19 00:38:20 +0700

    fix(method): :bug: fix return of `propertyEventDecorator` method
    add function before return `(target, propertyKey): void`

--- src/annotation/property_event_decorator.ts
+++ src/annotation/property_event_decorator.ts
@@ -3,15 +3,17 @@ import { hasOwnMetadata, defineMetadata } from '@abraham/reflection';
 import { Metadata } from '../planning/metadata';
 
 export function propertyEventDecorator(eventKey: string, errorMessage: string) {
-  return function (
-    target: { constructor: NewableFunction },
-    propertyKey: string
-  ) {
-    const metadata = new Metadata(eventKey, propertyKey);
+  return () => {
+    return function (
+      target: { constructor: NewableFunction },
+      propertyKey: string
+    ) {
+      const metadata = new Metadata(eventKey, propertyKey);
 
-    if (hasOwnMetadata(eventKey, target.constructor)) {
-      throw new Error(errorMessage);
-    }
-    defineMetadata(eventKey, metadata, target.constructor);
+      if (hasOwnMetadata(eventKey, target.constructor)) {
+        throw new Error(errorMessage);
+      }
+      defineMetadata(eventKey, metadata, target.constructor);
+    };
   };
 }
